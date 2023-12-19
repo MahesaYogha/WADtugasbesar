0cb70ae12d815c0a4530b4d2f5bb0fd6068a69aa
dpmarthin <dpmarthin@student.telkomuniversity.ac.id>
2023-12-19 21:34:32 +0700

    Upload Login

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,18 @@ Route::get('/review', [ReviewController::class, 'edit']);
 Route::get('/menu', [MenuController::class, 'index']);
 Route::get('/menu', [MenuController::class, 'create']);
 Route::get('/menu', [MenuController::class, 'edit']);
+
+Route::get('//', function () {
+    return view('welcome');
+})->middleware('auth');
+
+// Login
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'authenticate'])->name('auth.authenticate');
+
+// Register
+Route::get('/register', [AuthController::class, 'register'])->name('auth.register');
+Route::post('/register', [AuthController::class, 'store'])->name('auth.store');
+
+// Logout
+Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
